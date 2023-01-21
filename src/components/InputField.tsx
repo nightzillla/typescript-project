b87b677d0fd37c3092405205a02bd2d9e0559fd0
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-01-21 15:55:42 -0600

    adding input value in typescript

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import "./style.css"
 
-const InputField = () => { 
+interface Props {
+    todo:string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+const InputField: React.FC <Props> = ({ todo, setTodo }: Props) => {
+
   return (
     <form className='input'>
-        <input type = 'input' placeholder='Enter a task' className="input_box"/>
+        <input 
+        type = 'input'
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)} 
+        placeholder='Enter a task' 
+        className="input_box"/>
         <button className='input_sumbit' type='submit'>Go</button>
     </form>
   )
