e79392d5730e8a02c97b4719c0fad725f7fa88ce
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-01-21 17:19:47 -0600

    updating file for todo.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
 import { Todo } from './components/model';
+import TodoList from './components/TodoList';
 
 
 const App: React.FC = () => {
@@ -23,6 +24,10 @@ const App: React.FC = () => {
     <div className="App">
       <span className="heading">Taskify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList/>
+      {/* {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))} */}
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./style.css"
 
 interface Props {
@@ -7,10 +7,14 @@ interface Props {
     handleAdd: (e: React.FormEvent) => void;
 }
 const InputField: React.FC <Props> = ({ todo, setTodo, handleAdd }: Props) => {
-
+    const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <form className='input' onSubmit={handleAdd}>
+    <form className='input' onSubmit={(e) => {
+        handleAdd(e)
+        inputRef.current?.blur();
+        }}>
         <input 
+        ref={inputRef}
         type = 'input'
         value={todo}
         onChange={(e) => setTodo(e.target.value)} 
